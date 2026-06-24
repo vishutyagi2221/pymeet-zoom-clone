544ad1597d385d376ae585f55e725d6f87ddec8e
PyMeet Dev <pymeet@example.com>
2026-06-24 15:31:25 +0530

    Remove background and focus rings from emoji buttons

--- frontend/src/components/ReactionPicker.tsx
+++ frontend/src/components/ReactionPicker.tsx
@@ -28,7 +28,7 @@ export function ReactionPicker({ onSelectReaction }: { onSelectReaction: (emoji:
               onClick={() => {
                 onSelectReaction(emoji);
               }}
-              className="grid h-10 w-10 place-items-center rounded-full text-2xl transition-all hover:scale-125 hover:bg-slate-800 focus:outline-none focus:ring-2 focus:ring-primary"
+              className="grid h-10 w-10 place-items-center rounded-full text-2xl transition-all hover:scale-125 active:scale-90 focus:outline-none"
             >
               {emoji}
             </button>
